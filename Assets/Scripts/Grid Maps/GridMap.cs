20f2c88a2a03db2b856325088862680eb8fafe53
Nathan <150503N@mymail.nyp.edu.sg>
2017-06-09 15:31:50 +0800

    Made improvements to Grid, and added models from artist

--- Assets/Scripts/Grid Maps/GridMap.cs
+++ Assets/Scripts/Grid Maps/GridMap.cs
@@ -9,10 +9,10 @@ public class GridMap : MonoBehaviour {
     GameObject Grid_Empty;
     [SerializeField]
     GameObject Grid_Barricade;
-    public GameObject Grid_Barricade_Left;
     public GameObject Grid_Barricade_Front;
-    public GameObject Grid_Barricade_Right;
     public GameObject Grid_Barricade_Back;
+    public GameObject Grid_Barricade_Left;
+    public GameObject Grid_Barricade_Right;
 
     GameObject Grid_Parent;
     
@@ -20,35 +20,20 @@ public class GridMap : MonoBehaviour {
     public int i_GridSize_Y = 6;
 
     private GameObject[,] Grid;
-    Vector3 Offset;
+    Vector3 Offset, Front_Offset, Back_Offset, Left_Offset, Right_Offset;
    
   
 
     void Awake()
     {
         Grid = new GameObject[i_GridSize_X, i_GridSize_Y];
-        Offset = new Vector3((i_GridSize_X - 1) * -.125f, 0, (i_GridSize_Y - 1) * -.125f);
+        Offset = new Vector3((i_GridSize_X - 1) * -.5f, 0, (i_GridSize_Y - 1) * -.5f);
+        Front_Offset = new Vector3(-1, 0, 0);
+        Back_Offset = new Vector3(1, 0, 0);
+        Left_Offset = new Vector3(0, 0, -1);
+        Right_Offset = new Vector3(0, 0, 1);
 
-        for (int x = 0; x < i_GridSize_X; x++)
-        {
-            for (int z = 0; z < i_GridSize_Y; z++) 
-            {
-                if((x == 0 || x == i_GridSize_X -1) || (z == 0 || z == i_GridSize_Y - 1))
-                {
-                    GameObject gridplane = (GameObject)Instantiate(Grid_Barricade, new Vector3(x * 0.25f, 0, z * 0.25f) + Offset, Quaternion.identity);
-                    gridplane.gameObject.name = "Barricade";
-                    Grid[x, z] = gridplane;
-                }
-                else
-                {
-                    GameObject gridplane = (GameObject)Instantiate(Grid_Empty, new Vector3(x * 0.25f, 0, z * 0.25f) + Offset, Quaternion.identity);
-                    gridplane.gameObject.name = "Empty Grid";
-                    Grid[x, z] = gridplane;
-                }
-                
-            }
-        }
-        
+        BuildGrid();
     }
 
 	// Use this for initialization
@@ -63,6 +48,51 @@ public class GridMap : MonoBehaviour {
      
 
 	}
+
+    public void BuildGrid()
+    {
+        for (int x = 0; x < i_GridSize_X; x++)
+        {
+            for (int z = 0; z < i_GridSize_Y; z++)
+            {
+                //if((x == 0 || x == i_GridSize_X -1))
+                //{
+                //    GameObject gridplane = (GameObject)Instantiate(Grid_Barricade_Front, new Vector3(x * 0.25f, 0, z * 0.25f) + Offset, Quaternion.identity);
+                //    gridplane.gameObject.name = "Barricade";
+                //    Grid[x, z] = gridplane;
+                //}
+                //else if (z == 0 || z == i_GridSize_Y - 1)
+                //{
+                //    GameObject gridplane = (GameObject)Instantiate(Grid_Barricade_Left, new Vector3(x * 0.25f, 0, z * 0.25f) + Offset, Quaternion.identity);
+                //    gridplane.gameObject.name = "Barricade";
+                //    Grid[x, z] = gridplane;
+                //}
+                //else
+                {
+                    GameObject gridplane = (GameObject)Instantiate(Grid_Empty, new Vector3(x, 0, z) + Offset, Quaternion.identity);
+                    gridplane.gameObject.name = "Empty Grid";
+                    Grid[x, z] = gridplane;
+                }
+
+            }
+        }
+        Vector3 Front_Midpoint = Grid[0, 0].transform.position + ((Grid[0, i_GridSize_Y - 1].transform.position - Grid[0, 0].transform.position) / 2);
+        GameObject Front_3 = (GameObject)Instantiate(Grid_Barricade_Front, Front_Midpoint + Front_Offset, Quaternion.identity);
+        Front_3.gameObject.name = "Barrier";
+
+        Vector3 Back_Midpoint = Grid[i_GridSize_X - 1, 0].transform.position + ((Grid[i_GridSize_X - 1,i_GridSize_Y - 1].transform.position - Grid[i_GridSize_X - 1, 0].transform.position) / 2);
+        GameObject Back_3 = (GameObject)Instantiate(Grid_Barricade_Back, Back_Midpoint + Back_Offset, Quaternion.identity);
+        Back_3.gameObject.name = "Barrier";
+
+        Vector3 Left_Midpoint = Grid[0, 0].transform.position + ((Grid[i_GridSize_X - 1,0].transform.position - Grid[0, 0].transform.position) / 2);
+        GameObject Left_3 = (GameObject)Instantiate(Grid_Barricade_Left, Left_Midpoint + Left_Offset, Quaternion.identity);
+        Left_3.gameObject.name = "Barrier";
+
+        Vector3 Right_Midpoint = Grid[0, i_GridSize_Y - 1].transform.position + ((Grid[i_GridSize_X - 1, i_GridSize_Y - 1].transform.position - Grid[0, i_GridSize_Y - 1].transform.position) / 2);
+        GameObject Right_3 = (GameObject)Instantiate(Grid_Barricade_Right, Right_Midpoint + Right_Offset, Quaternion.identity);
+        Right_3.gameObject.name = "Barrier";
+
+    }
 
     public void ClamptoGrid(GameObject go)
     {
