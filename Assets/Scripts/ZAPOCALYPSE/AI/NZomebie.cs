eae7e1ae44c71ceb0d2759a1013eae014fc15d83
HighwindEasy <Kiraarmy15@gmail.com>
2017-07-31 17:45:58 +0800

    Attempted at fixing bugs

--- Assets/Scripts/ZAPOCALYPSE/AI/NZomebie.cs
+++ Assets/Scripts/ZAPOCALYPSE/AI/NZomebie.cs
@@ -43,7 +43,7 @@ public class NZomebie : Zombie {
         atkSpd = 0.2f;
         f_attackRate = atkSpd;
         this.moveSpd = 1.5f;
-        OriginPOint = GameObject.Find("CityTerrain");
+        OriginPOint = GameObject.Find("TownSpawn");
         this.transform.parent = OriginPOint.transform;
         this.moveSpd = Random.Range(1.0f, 2.5f);
         // this.gameObject.transform.parent = GameObject.Find("TerrainSpawn").transform;
@@ -91,11 +91,11 @@ public class NZomebie : Zombie {
                             source.PlayOneShot(zombieSound[Random.Range(0, 2)], 0.2F);
                         Vector3 dir = (go_targetedEnemy.transform.position - this.gameObject.transform.position).normalized;
                         //dir.y = 0;
-                        this.gameObject.transform.position += dir * moveSpd * Time.deltaTime;
+                        this.gameObject.transform.localPosition += dir * moveSpd * Time.deltaTime;
 
-                        //Quaternion lookRotation = Quaternion.LookRotation(dir);
-
-                        //this.gameObject.transform.rotation = Quaternion.Slerp(this.gameObject.transform.rotation, lookRotation, Time.deltaTime * 5);
+                        Quaternion lookRotation = Quaternion.LookRotation(dir);
+                        
+                        this.gameObject.transform.rotation = Quaternion.Slerp(this.gameObject.transform.rotation, lookRotation, Time.deltaTime * 5);
 
                     }
                     if (Distance < 1.5f)
@@ -110,11 +110,11 @@ public class NZomebie : Zombie {
                     float Distance = Vector3.Distance(this.gameObject.transform.position, go_targetedEnemy.gameObject.transform.position);
                     if (go_targetedEnemy != null && Distance < 1.5f) 
                     {
-                        //Vector3 dir = (go_targetedEnemy.transform.position - this.gameObject.transform.position).normalized;
-                        //dir.y = 0;
-                        //Quaternion lookRotation = Quaternion.LookRotation(dir);
+                        Vector3 dir = (go_targetedEnemy.transform.position - this.gameObject.transform.position).normalized;
+                        dir.y = 0;
+                        Quaternion lookRotation = Quaternion.LookRotation(dir);
 
-                        //this.gameObject.transform.rotation = Quaternion.Slerp(this.gameObject.transform.rotation, lookRotation, Time.deltaTime * 5);
+                        this.gameObject.transform.rotation = Quaternion.Slerp(this.gameObject.transform.rotation, lookRotation, Time.deltaTime * 5);
 
                         anim.SetTrigger("Attack");
                         AttackEnemy(go_targetedEnemy);
