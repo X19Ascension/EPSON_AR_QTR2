2d135627e771a88914f064d059a8e33e3590756a
HighwindEasy <Kiraarmy15@gmail.com>
2017-08-01 14:56:22 +0800

    Trying some terrain fixes

--- Assets/Scripts/ZAPOCALYPSE/MarkerFix(Temp)/tempfix.cs
+++ Assets/Scripts/ZAPOCALYPSE/MarkerFix(Temp)/tempfix.cs
@@ -2,18 +2,21 @@
 using System.Collections;
 
 public class tempfix : MonoBehaviour {
+    public GameObject Origin;
 
     GridMap thegrid;
 	// Use this for initialization
 	void Start () {
         thegrid = GameObject.Find("Game Manager").GetComponent<GridMap>();
-	
+        this.transform.parent = Origin.transform;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         //thegrid.ClamptoGrid(this.gameObject);
+       //this.transform.position = Origin.transform.position ;
+       //transform.localRotation = Origin.transform.rotation;
 	
 	}
 }
